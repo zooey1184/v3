579f98cedf120df981b7cf4fbb94169457d2c997
hyongnim <409337645@qq.com>
2018-10-28 18:01:52 +0800

    d

---
+++ deploy/p1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# npm run b
+NODE_ENV=production node deploy/tools/archive
+scp  dist.zip root@www.nizhenpi.com:/root/html
+ssh root@www.nizhenpi.com  << EOF
+    cd /root/html;
+    unzip -o -d h5 dist.zip;
+    exit;
+EOF
