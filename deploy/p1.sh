2b3e57a3ff31b3d59066394a33118901d10949be
hyongnim <409337645@qq.com>
2018-10-29 17:31:02 +0800

    p

--- deploy/p1.sh
+++ deploy/p1.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
-npm run b
+# npm run b
 NODE_ENV=production node deploy/tools/archive
 scp  dist.zip root@www.nizhenpi.com:/root/html
 ssh root@www.nizhenpi.com  << EOF
+    cd /root/html;
+    unzip -o -d h5 dist.zip;
+    exit;
+EOF
+
+scp  dist.zip root@www.newkouzi.com:/root/html
+ssh root@www.newkouzi.com  << EOF
+    cd /root/html;
+    unzip -o -d zx dist.zip;
+    exit;
+EOF
+
+scp  dist.zip root@www.ml-credit.com:/root/html
+ssh root@www.ml-credit.com  << EOF
     cd /root/html;
     unzip -o -d h5 dist.zip;
     exit;
